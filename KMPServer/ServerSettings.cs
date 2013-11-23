a502325f8ea7ac6ebd7e44d0414a72399d729af1
Ryan Shaw <ryannathans@hotmail.com>
2013-11-24 01:30:24 +0800

    Playing around, learning git commands
    Revert "Cleaned up indentation. (it compiled fine but otherwise untested)"
    
    This reverts commit 8b7dfb035daecfb156473d299aa6b2e6fa226fe1.

--- KMPServer/ServerSettings.cs
+++ KMPServer/ServerSettings.cs
@@ -14,7 +14,7 @@ namespace KMPServer
 		public const String SERVER_CONFIG_FILENAME = "KMPServerConfig.txt";
 		public const string SERVER_WHITELIST_FILENAME = "KMPWhitelist.txt";
 		public const string SERVER_BANS_FILENAME = "KMPBans.txt";
-		public const string SERVER_ADMINS_FILENAME = "KMPAdmins.txt";
+        public const string SERVER_ADMINS_FILENAME = "KMPAdmins.txt";
 
 		public class BanRecord
 		{
@@ -32,7 +32,7 @@ namespace KMPServer
 			public string ipBinding = "0.0.0.0";
 			public int port = 2076;
 			public int httpPort = 8081;
-			public bool httpBroadcast = true;
+            public bool httpBroadcast = true;
 			public int maxClients = 8;
 			public float updatesPerSecond = 60;
 			public int screenshotInterval = 3000;
@@ -50,36 +50,35 @@ namespace KMPServer
 			public int screenshotHeight = 600;
 			public bool profanityFilter = true;
 			public double safetyBubbleRadius = 20000d;
-			public bool autoDekessler = false;
-			public int autoDekesslerTime = 30;
-			public string profanityWords = "fucker:kerper,faggot:kerpot,shit:kerp,fuck:guck,cunt:kump,piss:heph,fag:olp,dick:derp,cock:beet,asshole:hepderm,nigger:haggar";
+            public bool autoDekessler = false;
+            public int autoDekesslerTime = 30;
+            public string profanityWords = "fucker:kerper,faggot:kerpot,shit:kerp,fuck:guck,cunt:kump,piss:heph,fag:olp,dick:derp,cock:beet,asshole:hepderm,nigger:haggar";
 
             private IEnumerable<KeyValuePair<string, string>> _profanity = null;
             public IEnumerable<KeyValuePair<string, string>> Profanity
-			{
-        		get
-        		{
-            		if (_profanity == null)
-           			{
-                		try
-                		{
-                    		//Uses a Enumerable KVP instead of a dictionary to avoid an extra conversion.
-                    		_profanity = profanityWords.Split(',').Select(ws =>
-                    		{
-                        		var wx = ws.Split(':');
-                        		return new KeyValuePair<string, string>(wx[0], wx[1]);
-                    		});
-                		}
+            {
+                get
+                {
+                    if (_profanity == null)
+                    {
+                        try
+                        {
+                            //Uses a Enumerable KVP instead of a dictionary to avoid an extra conversion.
+                            _profanity = profanityWords.Split(',').Select(ws =>
+                            {
+                                var wx = ws.Split(':');
+                                return new KeyValuePair<string, string>(wx[0], wx[1]);
+                            });
+                        }
+                        catch
+                        {
+                            _profanity = new Dictionary<string, string>();
+                        }
+                    }
 
-                		catch
-                		{
-                    		_profanity = new Dictionary<string, string>();
-                		}
-            		}
-
-            		return _profanity;
-        		}
-			}
+                    return _profanity;
+                }
+            }
 
 			private List<BanRecord> _bans = new List<BanRecord>();
 			internal List<BanRecord> bans
@@ -99,14 +98,14 @@ namespace KMPServer
 				}
 			}
 
-		    private List<string> _admins = new List<string>();
-		    internal List<string> admins
-		    {
-		        get
-		        {
-		            return _admins;
-		        }
-		    }
+            private List<string> _admins = new List<string>();
+            internal List<string> admins
+            {
+                get
+                {
+                    return _admins;
+                }
+            }
 
 			private ScreenshotSettings _screenshotSettings = new ScreenshotSettings();
 			internal ScreenshotSettings screenshotSettings
@@ -177,7 +176,6 @@ namespace KMPServer
 
 				f.SetValue(Store, newValue);
 			}
-
 			catch
 			{
 				throw new ArgumentException(string.Format("{0} is not a valid value for {1}", Value, Key));
@@ -218,7 +216,7 @@ namespace KMPServer
 					}
 				}
 			}
-			catch { }//empty? Really?
+			catch { }
 		}
 
 		public static void loadBans(ConfigStore Store)
@@ -262,7 +260,7 @@ namespace KMPServer
 					}
 				}
 			}
-			catch { } //Empty? Really?
+			catch { }
 		}
 
 		public static void saveWhitelist(ConfigStore Store)
@@ -284,7 +282,7 @@ namespace KMPServer
 					}
 				}
 			}
-			catch { } //Empty? Really?
+			catch { }
 		}
 
 		public static void loadWhitelist(ConfigStore Store)
@@ -299,7 +297,7 @@ namespace KMPServer
 					Store.whitelist.AddRange(File.ReadAllLines(FileName));
 				}
 			}
-			catch { } //Are we really using Try/Catch effectively? 
+			catch { }
 		}
 
         public static void saveAdmins(ConfigStore Store)
@@ -321,7 +319,7 @@ namespace KMPServer
                     }
                 }
             }
-            catch { } //...
+            catch { }
         }
 			
 
@@ -337,7 +335,7 @@ namespace KMPServer
                     store.admins.AddRange(File.ReadAllLines(fileName));
                 }
             }
-            catch { } //Haven't seen this done before
+            catch { }
         }
 
 		//Write the setting store out to a file by reflecting over its members.
@@ -362,7 +360,7 @@ namespace KMPServer
 					}
 				}
 			}
-			catch (Exception) //Ooh, that's a little different. Why go to the effort to write Exception but not a debug/output line?
+			catch (Exception)
 			{
 
 			}
@@ -456,7 +454,7 @@ namespace KMPServer
 			}
 			catch (Exception)
 			{
-			// I understand you might sometimes want to leave one blank, but comment it when done. Just don't leave them all blank. It's frustrating to debug a project that's in development when 99% of the catches are empty
+
 			}
 
 			foreach (FieldInfo f in Store.GetType().GetFields())
