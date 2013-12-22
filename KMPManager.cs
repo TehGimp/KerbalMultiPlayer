e56954bb67e4aa1a78c729cd71490ed40a85d8b8
Shaun C Esau <shaunesau@gmail.com>
2013-12-21 22:01:06 -0700

    Re: #455, let's try explicitly setting InUse=false and IsMine=True on an undock

--- KMPManager.cs
+++ KMPManager.cs
@@ -3004,6 +3004,8 @@ namespace KMP
 			{
 				serverVessels_PartCounts[data.vessel.id] = 0;
 				serverVessels_ProtoVessels.Remove(data.vessel.id);
+				serverVessels_InUse[data.vessel.id] = false;
+				serverVessels_IsMine[data.vessel.id] = true;
 				sendVesselMessage(data.vessel,true);
 			}
 			//Invoke("setFinishDocking",1f);
