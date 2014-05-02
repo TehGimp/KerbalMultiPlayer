9acd72af3d545692ef2536ec86c163c496062bf9
Shaun C Esau <shaunesau@gmail.com>
2014-05-02 15:00:30 -0600

    Fix some minor input issues

--- KMPServer/Server.cs
+++ KMPServer/Server.cs
@@ -783,7 +783,6 @@ namespace KMPServer
 
         private void processCommand(String input)
         {
-            Log.Info("Command Input: {0}", input);
             try
             {
                 String cleanInput = input.ToLower().Trim();
